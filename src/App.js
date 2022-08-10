50ee9cc6b87c54703ec7e723674a1e970fddbb51
Debashis Nandi <debashisnandi576@gmail.com>
2022-08-11 03:23:46 +0530

    added the score and highscore functionality

--- src/App.js
+++ src/App.js
@@ -1,163 +1,22 @@
-import React, { Component } from 'react'
-import Food from './components/Food'
-import Snake from './components/Snake'
-
-const getRandomCoordinates = () => {
-  let min = 1;
-  let max = 98;
-  let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
-  let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
-  return [x, y];
-}
-
-const initialState=
-{
-  food: getRandomCoordinates(),
-  speed:200,
-  direction: 'RIGHT',
-  snakeDots: [
-    [0, 0],
-    [2, 0]
-  ]
-
-}
-
+import React, { Component } from 'react';
+import GameBoard from './components/GameBoard';
+import { BrowserRouter, Route,Routes} from "react-router-dom";
+import Header from './components/Header';
+import MainScreen from './components/MainScreen';
+import './App.css';
 export default class App extends Component {
-
-  state =initialState;
-
-  componentDidMount() 
-  {
-    setInterval(this.moveSnake,this.state.speed);
-    document.onkeydown = this.onKeyDown;
-  }
-
-  componentDidUpdate()
-  {
-    this.checkIfOutOfBorders();
-    this.checkIfCollapsed();
-    this.checkIfFoodEaten();
-  }
-
-  onKeyDown = (e) => {
-    e = e || window.event;
-    switch (e.keyCode) {
-      case 38:
-        this.setState({ direction: 'UP' });
-        break;
-      case 40:
-        this.setState({ direction: 'DOWN' });
-        break;
-      case 37:
-        this.setState({ direction: 'LEFT' });
-        break;
-      case 39:
-        this.setState({ direction: 'RIGHT' });
-        break;
-      default:
-
-    }
-  }
-
-  moveSnake = () => {
-    let dots = [...this.state.snakeDots];
-    let head = dots[dots.length - 1];
-
-
-    switch (this.state.direction) {
-      case 'RIGHT':
-        head = [head[0] + 2, head[1]];
-        break;
-      case 'LEFT':
-        head = [head[0] - 2, head[1]];
-        break;
-      case 'UP':
-        head = [head[0], head[1]-2];
-        break;
-      case 'DOWN':
-        head = [head[0], head[1]+2];
-        break;
-      default:
-
-    }
-    dots.push(head);
-    dots.shift();
-    this.setState({snakeDots:dots})
-  }
-  checkIfOutOfBorders=()=>
-  {
-    let head=this.state.snakeDots[this.state.snakeDots.length-1];
-    if(head[0]>=100 || head[1]>=100 || head[0]<0 || head[1]<0)
-    {
-      this.onGameOver();
-    }
-  }
-
-  checkIfCollapsed()
-  {
-    let snake=[...this.state.snakeDots];
-    let head=snake[snake.length-1];
-    snake.pop();
-    snake.forEach(dot=>
-      {
-        if(head[0]===dot[0] && head[1]===dot[1])
-        {
-          this.onGameOver();
-        }
-      })
-  }
-
-  checkIfFoodEaten()
-  {
-    let head=this.state.snakeDots[this.state.snakeDots.length-1];
-    let food=this.state.food;
-    if(head[0]===food[0] && head[1]===food[1])
-    {
-      this.setState(
-        {
-          food:getRandomCoordinates()
-        }
-      )
-      this.enlargeSnake();
-      this.increaseSpeed();
-    }
-  }
-  enlargeSnake()
-  {
-    let newSnake=[...this.state.snakeDots];
-    newSnake.unshift([]);
-    this.setState(
-      {
-        snakeDots:newSnake
-      }
-    )
-  }
-  increaseSpeed()
-  {
-    if(this.state.speed>10)
-    {
-      this.setState(
-        {
-          speed: this.state.speed-10
-        }
-      )
-    }
-
-  }
-  onGameOver()
-  {
-    alert(`Game is OVER. Snake length is ${this.state.snakeDots.length}`);
-    this.setState(initialState);
-  }
-
-
-
-
   render() {
     return (
-      <div className='game-area'>
-        <Snake snakeDots={this.state.snakeDots} />
-        <Food dot={this.state.food} />
+      <div className='body'>
+        <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route exact path='/' element={<MainScreen/>}></Route>
+          <Route path='/gameboardeasy' element={<GameBoard speed={200} level={'easy'}/>}></Route>
+          <Route path='/gameboardmedium' element={<GameBoard speed={100} level={'medium'}/>}></Route>
+          <Route path='/gameboardhard' element={<GameBoard speed={55} level={'hard'}/>}></Route>
+        </Routes>
+        </BrowserRouter>
       </div>
     )
   }
