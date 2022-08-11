fd864899c8fe14183f942c837e285c36f74cab71
Debashis Nandi <debashisnandi576@gmail.com>
2022-08-11 21:24:26 +0530

    added the functionality to reset the highscore of each level individually and reset all high scores and changed the styling of these buttons

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -3,43 +3,60 @@ import { Link } from 'react-router-dom';
 import './MainScreen.css';
 
 export default class MainScreen extends Component {
-  easyhscore=localStorage.getItem('easyhscore');
-  mediumhscore=localStorage.getItem('mediumhscore');
-  hardhscore=localStorage.getItem('hardhscore');
-  handleEasyReset()
-  {
-    console.log("btn clicked")
-  }
-  handleMediumReset()
-  {
-    
-  }
+  easyhscore = localStorage.getItem('easyhscore');
+  mediumhscore = localStorage.getItem('mediumhscore');
+  hardhscore = localStorage.getItem('hardhscore');
   
+
+  handleEasyReset() 
+  {
+    localStorage.setItem('easyhscore',0);
+    window.location.reload(false);
+  }
+  handleMediumReset() 
+  {
+    localStorage.setItem('mediumhscore',0);
+    window.location.reload(false);
+  }
+  handleHardReset()
+  {
+    localStorage.setItem('hardhscore',0);
+    window.location.reload(false);
+  }
+  handleAllReset()
+  {
+    localStorage.setItem('easyhscore',0);
+    localStorage.setItem('mediumhscore',0);
+    localStorage.setItem('hardhscore',0);
+    window.location.reload(false);
+  }
+
+
   render() {
     return (
       <div className='mainscreen'>
         <div className='level'>
           <h2>Choose Level</h2>
-          <Link to='/gameboardeasy' style={{textDecoration:'none'}}><p>Easy</p></Link>
-          <Link to='/gameboardmedium' style={{textDecoration:'none'}}><p>Medium</p></Link>
-          <Link to='/gameboardhard' style={{textDecoration:'none'}}><p>Hard</p></Link>
+          <Link to='/gameboardeasy' style={{ textDecoration: 'none' }}><p>Easy</p></Link>
+          <Link to='/gameboardmedium' style={{ textDecoration: 'none' }}><p>Medium</p></Link>
+          <Link to='/gameboardhard' style={{ textDecoration: 'none' }}><p>Hard</p></Link>
         </div>
         <div className='scorecard'>
           <h2>Highest Scores</h2>
           <div className='hscore-div'>
-            <p className='levelinfo'>Easy level: {(this.easyhscore==null)?0:(parseInt(JSON.parse(this.easyhscore)))}</p>
+            <p className='levelinfo'>Easy level: {(this.easyhscore == null) ? 0 : (parseInt(JSON.parse(this.easyhscore)))}</p>
             <button className='resetbtn' onClick={this.handleEasyReset}>Reset this highscore</button>
           </div>
           <div className='hscore-div'>
-            <p className='levelinfo'>Medium level: {(this.mediumhscore==null)?0:(parseInt(JSON.parse(this.mediumhscore)))}</p>
+            <p className='levelinfo'>Medium level: {(this.mediumhscore == null) ? 0 : (parseInt(JSON.parse(this.mediumhscore)))}</p>
             <button className='resetbtn' onClick={this.handleMediumReset}>Reset this highscore</button>
           </div>
           <div className='hscore-div'>
-            <p className='levelinfo'>Hard level: {(this.hardhscore==null)?0:(parseInt(JSON.parse(this.hardhscore)))}</p>
-            <button className='resetbtn'>Reset this highscore</button>
+            <p className='levelinfo'>Hard level: {(this.hardhscore == null) ? 0 : (parseInt(JSON.parse(this.hardhscore)))}</p>
+            <button className='resetbtn' onClick={this.handleHardReset}>Reset this highscore</button>
           </div>
           <div className='resetall'>
-            <button className='resetallbtn'>Reset all highscores</button>
+            <button className='resetallbtn' onClick={this.handleAllReset}>Reset all highscores</button>
           </div>
         </div>
       </div>
